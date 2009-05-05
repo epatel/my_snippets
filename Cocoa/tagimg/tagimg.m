8527748b671f56d4095d116fe2a4ce2287485208
Edward Patel <epatel@Paradox.local>
2009-05-05 12:11:23 +0200

    tagimg.m check for .png extension

--- Cocoa/tagimg/tagimg.m
+++ Cocoa/tagimg/tagimg.m
@@ -9,17 +9,25 @@
 
 int main (int argc, const char * argv[]) {
     [NSApplication sharedApplication];
-
+	
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
+	
     if (argc == 3) {
-
-        NSImage *image = [[NSImage alloc] initWithContentsOfFile:[NSString stringWithFormat:@"%s", argv[1]]];
-
+		
+		NSString *filename = [NSString stringWithFormat:@"%s", argv[1]];
+		NSString *extension = [filename pathExtension]; 
+		
+		if ([extension caseInsensitiveCompare:@"png"] != NSOrderedSame) {			
+			printf("Image file should be a .png file\n");
+			return -1;
+		}
+		
+        NSImage *image = [[NSImage alloc] initWithContentsOfFile:filename];
+		
         if (image) {
-
+			
             [image lockFocus];
-
+			
             NSString *string = [NSString stringWithFormat:@"%@\n%s", [NSDate date], argv[2]];
             NSShadow *shadow = [[[NSShadow alloc] init] autorelease];
             
@@ -29,37 +37,38 @@ int main (int argc, const char * argv[]) {
             
             NSMutableDictionary *attributes = [NSMutableDictionary dictionary];
             [attributes setObject:[NSColor whiteColor]
-                forKey:NSForegroundColorAttributeName];
+						   forKey:NSForegroundColorAttributeName];
             [attributes setObject:[NSFont fontWithName:@"Andale Mono" size:14] 
-                forKey:NSFontAttributeName];
+						   forKey:NSFontAttributeName];
             [attributes setObject:shadow
-                forKey:NSShadowAttributeName];
-
+						   forKey:NSShadowAttributeName];
+			
             [string drawAtPoint:NSMakePoint(10, 10) withAttributes:attributes];
-
+			
             [image unlockFocus];
-
+			
             NSBitmapImageRep *bits = [NSBitmapImageRep imageRepWithData:[image TIFFRepresentation]];
-
+			
             NSData *data = [bits representationUsingType:NSPNGFileType 
-                properties:nil];
-
-            [data writeToFile:[NSString stringWithFormat:@"%s", argv[1]]
-                atomically:YES];
-
+											  properties:nil];
+			
+            [data writeToFile:filename atomically:YES];
+			
         } else {
             
             printf("Could not open file '%s'\n", argv[1]);
+			return -1;
             
         }
         
     } else {
         
-        printf("Usage: tagimg <filename> <text>\n");
-        
+        printf("Usage: tagimg <filename.png> <text>\n");
+		return -1;
+		
     }
-
+	
     [pool drain];
-
+	
     return 0;
 }
